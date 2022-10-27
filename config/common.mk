9324211bf3d81e304074987c9b0b484a6afc4c2c
markakash <akashniki@gmail.com>
2022-10-27 14:59:01 +0000

    dex changes
    Change-Id: I16cbb0239968b97d2933fdb55177771477eee84f
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -205,6 +205,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# Inherit art options
+include vendor/aosp/config/art.mk
+
 # Repainter integration
 PRODUCT_PACKAGES += \
     RepainterServicePriv
